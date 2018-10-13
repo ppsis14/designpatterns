fb6940aa48cae4a6d52fd10fc6c6b2e54b44f833
thikamporn <thikamporn.s@ku.th>
2018-10-13 10:05:58 +0700

    add lab 9 coding in tour, edit package tour test

--- src/lab9Tour/Main.java
+++ src/lab9Tour/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         PackageTour tourBangkok = new PackageTour("Wat In Bangkok");
         tourBangkok.addSingleTour(new SingleTour("Wat Pra Kaew", 150, 10, 5));
-        tourBangkok.addSingleTour(new SingleTour("Float Market", 150, 12, 5));
+        tourBangkok.addSingleTour(new SingleTour("Floating Market", 150, 12, 5));
 
         System.out.println("Tour package name : " + tourBangkok.getName());
         System.out.println("Total price : " + tourBangkok.getPrice());
--- src/lab9Tour/PackageTourTest.java
+++ src/lab9Tour/PackageTourTest.java
@@ -1,24 +1,31 @@
 package lab9Tour;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 class PackageTourTest {
-    PackageTour tourBangkok = new PackageTour("Wat In Bangkok");
+    private PackageTour tourBangkok;
 
-    @Test
-    void checkName() {
-        Assertions.assertEquals("Wat In Bangkok = ", tourBangkok.getName());
+    @BeforeEach
+    public void setUp(){
+        tourBangkok = new PackageTour("Wat In Bangkok");
+        tourBangkok.addSingleTour(new SingleTour("Wat Pra Kaew", 150, 10, 5));
+        tourBangkok.addSingleTour(new SingleTour("Floating Market", 150, 12, 5));
     }
 
     @Test
-    void checkPrice() {
-        Assertions.assertEquals(0, tourBangkok.getPrice());
+    public void checkName() {
+        Assertions.assertEquals("Wat In Bangkok = Wat Pra Kaew : Floating Market : ", tourBangkok.getName());
     }
 
     @Test
-    void checkAvailableSeats() {
-        Assertions.assertEquals(100, tourBangkok.getAvailableSeats());
+    public void checkPrice() {
+        Assertions.assertEquals(270, tourBangkok.getPrice());
+    }
+
+    @Test
+    public void checkAvailableSeats() {
+        Assertions.assertEquals(5, tourBangkok.getAvailableSeats());
     }
 }
